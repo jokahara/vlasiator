2d75db6b33a8693b50724624ce5bf992dc82f7e7
Kähärä Jaakko O <jokahara@lx7-fuxi253.ad.helsinki.fi>
2019-06-19 15:37:19 +0300

    uses 50% data compression with log function

--- compression/log_compression.h
+++ compression/log_compression.h
@@ -1,8 +1,8 @@
 #include <cmath>
-
+#include <iostream>
 // Quite fast, 50 % float compressing with small error.
 // Designed to work for values between 1E-3 - 1E-15.
-class LogCompressedValue
+class CompressedValue
 {
     private:
         typedef union {     
@@ -12,43 +12,80 @@ class LogCompressedValue
         unsigned short compressed_value;
         
     public:
-        LogCompressedValue();
-        LogCompressedValue(float x);
+        CompressedValue();
+        CompressedValue(float x);
         float get() const;
         void set(float x);
-        
+        //static float *decompressArray(CompressedValue *array, int size);
+
         operator float() { return get(); }
         operator float() const { return get(); }
 
-        LogCompressedValue& operator=(float value) { set(value); return *this; }
-        LogCompressedValue& operator+=(float value) { set(get() + value); return *this; }
+        CompressedValue& operator=(float value) { set(value); return *this; }
+        CompressedValue& operator+=(float value) { set(get() + value); return *this; }
         
-        LogCompressedValue& operator=(const LogCompressedValue& value) { 
+        CompressedValue& operator=(const CompressedValue& value) { 
             compressed_value = value.compressed_value; 
             return *this;    
         }
-        LogCompressedValue& operator+=(LogCompressedValue value) { 
+        CompressedValue& operator+=(CompressedValue value) { 
             set(get() + value.get()); 
             return *this;    
         }
+        CompressedValue& operator*=(CompressedValue value) { 
+            float_int v1 { .i = 0xC00000FE }, v2 { .i = 0xC00000FE };
+            v1.i |= compressed_value << 9;
+            v2.i |= value.compressed_value << 9;
+
+            v1.f += v2.f;
+            compressed_value = v1.i >> 9;
+            return *this;
+        }
+        CompressedValue& operator/=(CompressedValue value) { 
+            float_int v1 { .i = 0xC00000FE }, v2 { .i = 0xC00000FE };
+            v1.i |= compressed_value << 9;
+            v2.i |= value.compressed_value << 9;
+
+            v1.f -= v2.f;
+            compressed_value = v1.i >> 9;
+            return *this;
+        }
 };
 
-inline LogCompressedValue::LogCompressedValue() { compressed_value = 0; }
+inline CompressedValue::CompressedValue() { compressed_value = 0; }
 
-inline LogCompressedValue::LogCompressedValue(float x) { set(x); }
+inline CompressedValue::CompressedValue(float x) { set(x); }
 
-inline void LogCompressedValue::set(float x)
+inline void CompressedValue::set(float x)
 {
-    float_int fi { .f = x ? log10f(x) : 0 };
-    compressed_value = fi.i >> 9; 
+    if (x < 1E-17f)
+    {
+        compressed_value = 0;
+    }
+    else
+    {
+        float_int fi { .f = log10f(x) };
+        compressed_value = fi.i >> 9; 
+    }   
 }
 
-inline float LogCompressedValue::get() const
+inline float CompressedValue::get() const
 {
-    float_int fi { .i = 3 << 30};
+    if (compressed_value == 0) return 0.0f;
+
+    float_int fi { .i = 0xC00000FE };
     fi.i |= compressed_value << 9;
-    fi.i |= 0xFE;                   // average error correction;
 
     return exp10f(fi.f);
 }
-
+/* 
+inline float *CompressedValue::decompressArray(CompressedValue *array, int size){
+    float cp[size];
+    for (int i = 0; i < size; i++)
+    {
+        cp[i] = array[i];
+    }
+    
+    return cp;
+}
+*/
