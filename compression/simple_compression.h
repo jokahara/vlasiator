ff2c481ed17a75046f68902759e2c0100cc0de92
Kähärä Jaakko O <jokahara@lx7-fuxi253.ad.helsinki.fi>
2019-06-19 15:41:20 +0300

    uses simple 25% data compression

--- compression/simple_compression.h
+++ compression/simple_compression.h
@@ -1,7 +1,7 @@
 
 
-// 
-class SimplyCompressedValue
+// Simple and fast 25 % compression
+class CompressedValue
 {
     private:
         typedef union {
@@ -9,45 +9,61 @@ class SimplyCompressedValue
             long i;
         } float_int;
 
-        typedef struct
-        {
-            unsigned char v1: 8;
-            unsigned char v2: 8;
-            unsigned char v3: 8;
-        } simple;
-        
-        simple val;
+        unsigned char v1: 8;
+        unsigned char v2: 8;
+        unsigned char v3: 8;
         
     public:
-        SimplyCompressedValue();
-        SimplyCompressedValue(float x);
-        float get();
+        CompressedValue();
+        CompressedValue(float x);
+        float get() const;
         void set(float x);
+
+        operator float() { return get(); }
+        operator float() const { return get(); }
+
+        CompressedValue& operator=(float value) { set(value); return *this; }
+        CompressedValue& operator+=(float value) { set(get() + value); return *this; }
+        CompressedValue& operator*=(float value) { set(get() * value); return *this; }
+        
+        CompressedValue& operator=(const CompressedValue& value) { 
+            v1 = value.v1;
+            v2 = value.v2;
+            v3 = value.v3;
+            return *this;    
+        }
+        CompressedValue& operator+=(CompressedValue value) { 
+            set(get() + value.get()); 
+            return *this;    
+        }
 };
 
-inline SimplyCompressedValue::SimplyCompressedValue()
+inline CompressedValue::CompressedValue()
 {
-    val.v1 = val.v2 = 0;
+    v1 = v2 = v3 = 0;
 }
 
-inline SimplyCompressedValue::SimplyCompressedValue(float x)
+inline CompressedValue::CompressedValue(float x)
 {
     set(x);
 }
 
-inline void SimplyCompressedValue::set(float x)
+inline void CompressedValue::set(float x)
 {
     float_int fi { .f = x };
-    val.v1 = fi.i >> 6;
-    val.v2 = fi.i >> 14;
-    val.v3 = fi.i >> 22;
+    // byte shifts and stores float in to three short values
+    v1 = fi.i >> 6;
+    v2 = fi.i >> 14;
+    v3 = fi.i >> 22;
 }
 
-inline float SimplyCompressedValue::get()
+inline float CompressedValue::get() const
 {
-    float_int fi { .i = val.v3 << 22 };
-    fi.i |= val.v2 << 14;
-    fi.i |= val.v1 << 6;
+    if (v3 == 0) return 0.f; 
+
+    float_int fi { .i = v3 << 22 };
+    fi.i |= v2 << 14;
+    fi.i |= v1 << 6;
     fi.i |= 32;                   // average error correction;
 
     return fi.f;
