38b0590eabc538cc6171ffb023959aa6431f1997
Kähärä Jaakko O <jokahara@lx7-fuxi253.ad.helsinki.fi>
2020-03-18 13:10:38 +0200

    testing cpu_trans_map.cpp

--- vlasovsolver/cpu_trans_map.cpp
+++ vlasovsolver/cpu_trans_map.cpp
@@ -647,8 +647,7 @@ void update_remote_mapping_contribution(
    vector<CellID> receive_cells;
    vector<CellID> m_cells;
    vector<CellID> send_cells;
-   vector<Realf*> receiveBuffers;
-   vector<Compf*> compBuffers;
+   vector<Compf*> receiveBuffers;
 
 //    int myRank;   
 //    MPI_Comm_rank(MPI_COMM_WORLD,&myRank);
@@ -665,14 +664,11 @@ void update_remote_mapping_contribution(
       //default values, to avoid any extra sends and receives
       for (uint i = 0; i < MAX_NEIGHBORS_PER_DIM; ++i) {
          if(i == 0) {
-            ccell->neighbor_block_data.at(i) = ccell->get_data(popID);
             ccell->neighbor_compressed_data.at(i) = ccell->get_compressed_data(popID);
          } else {
             ccell->neighbor_compressed_data.at(i) = NULL;
-            ccell->neighbor_block_data.at(i) = NULL;
          }
          ccell->neighbor_compressed_size.at(i) = 0;
-         ccell->neighbor_number_of_blocks.at(i) = 0;
       }
    }
 
@@ -683,14 +679,11 @@ void update_remote_mapping_contribution(
       //default values, to avoid any extra sends and receives
       for (uint i = 0; i < MAX_NEIGHBORS_PER_DIM; ++i) {
          if(i == 0) {
-            ccell->neighbor_block_data.at(i) = ccell->get_data(popID);
             ccell->neighbor_compressed_data.at(i) = ccell->get_compressed_data(popID);
          } else {
-            ccell->neighbor_block_data.at(i) = NULL;
             ccell->neighbor_compressed_data.at(i) = NULL;
          }
          ccell->neighbor_compressed_size.at(i) = 0;
-         ccell->neighbor_number_of_blocks.at(i) = 0;
       }
       CellID p_ngbr = INVALID_CELLID;
       CellID m_ngbr = INVALID_CELLID;
@@ -721,8 +714,6 @@ void update_remote_mapping_contribution(
             //mapped to if 1) it is a valid target,
             //2) is remote cell, 3) if the source cell in center was
             //translated
-            //ccell->neighbor_block_data[0] = pcell->get_data(popID);
-            //ccell->neighbor_number_of_blocks[0] = pcell->get_number_of_velocity_blocks(popID);
 
             pcell->compress_data(popID);
             ccell->neighbor_compressed_data[0] = pcell->get_compressed_data(popID);
@@ -735,27 +726,10 @@ void update_remote_mapping_contribution(
          //Receive data that mcell mapped to ccell to this local cell
          //data array, if 1) m is a valid source cell, 2) center cell is to be updated (normal cell) 3) m is remote
          //we will here allocate a receive buffer, since we need to aggregate values
-         //mcell->neighbor_compressed_data[0] = (Compf*) aligned_malloc(ccell->get_number_of_velocity_blocks(popID) * (WID3+2) * sizeof(Compf), 1);
-         
-         //mcell->neighbor_number_of_blocks[0] = ccell->get_number_of_velocity_blocks(popID);
-         //mcell->neighbor_block_data[0] = (Realf*) aligned_malloc(mcell->neighbor_number_of_blocks[0] * WID3 * sizeof(Realf), 64);
-         
          receive_cells.push_back(local_cells[c]);
-         //receiveBuffers.push_back(mcell->neighbor_block_data[0]);
          m_cells.push_back(m_ngbr);
-         //receiveBuffers.push_back(mcell->neighbor_compressed_data[0]);
       }
    }
-   if (send_cells.size() > 0 )
-   {
-      std::cerr << "send sizes: ";
-      for (size_t i = 0; i < send_cells.size(); i++)
-      {
-         std::cerr << mpiGrid[send_cells[i]]->get_compressed_size(popID) << " ";
-      }
-      std::cerr << "\n";
-   }
-   else std::cerr << "receive\n";
 
    // Do communication
    SpatialCell::setCommunicatedSpecies(popID);
@@ -774,24 +748,14 @@ void update_remote_mapping_contribution(
       if(direction < 0) mpiGrid.update_copies_of_remote_neighbors(SHIFT_M_Z_NEIGHBORHOOD_ID);
       break;
    }
-   
-   if (receive_cells.size() > 0 )
-   {
-      std::cerr << "received sizes: ";
-      for (size_t i = 0; i < m_cells.size(); i++)
-      {
-         std::cerr << mpiGrid[m_cells[i]]->neighbor_compressed_size[0] << " ";
-      }
-      std::cerr << "\n";
-   }
-   else std::cerr << "send data\n";
 
    for (uint c = 0; c < m_cells.size(); c++)
    {
       SpatialCell* mcell = mpiGrid[m_cells[c]];
       mcell->neighbor_compressed_data[0] = (Compf*) aligned_malloc(mcell->neighbor_compressed_size[0] * sizeof(Compf), 1);
-      compBuffers.push_back(mcell->neighbor_compressed_data[0]);
+      receiveBuffers.push_back(mcell->neighbor_compressed_data[0]);
    }
+
    SpatialCell::set_mpi_transfer_type(Transfer::NEIGHBOR_COMP_DATA);
    switch(dimension) {
    case 0:
@@ -807,13 +771,12 @@ void update_remote_mapping_contribution(
       if(direction < 0) mpiGrid.update_copies_of_remote_neighbors(SHIFT_M_Z_NEIGHBORHOOD_ID);
       break;
    }
-   
-   std::cerr << "done\n";
+
    for (size_t c=0; c < receive_cells.size(); ++c) {
       SpatialCell* spatial_cell = mpiGrid[receive_cells[c]];
       Realf *blockData = spatial_cell->get_data(popID);
       
-      Compf* p = compBuffers[c];
+      Compf* p = receiveBuffers[c];
       vmesh::LocalID numberOfBlocks = spatial_cell->get_number_of_velocity_blocks(popID);
       uint32_t size[numberOfBlocks];
       uint32_t idx[numberOfBlocks];
@@ -832,10 +795,8 @@ void update_remote_mapping_contribution(
       }
    }
    
-   std::cerr << "clear\n";
 #pragma omp parallel
    {
-      
       // send cell data is set to zero. This is to avoid double copy if
       // one cell is the neighbor on bot + and - side to the same
       // process
@@ -851,19 +812,19 @@ void update_remote_mapping_contribution(
          }
       }
    }
-   std::cerr << "free\n";
+   
    //and finally free temporary receive buffer
    for (size_t c=0; c < receiveBuffers.size(); ++c) {
-      //aligned_free(receiveBuffers[c]);
-      aligned_free(compBuffers[c]);
+      aligned_free(receiveBuffers[c]);
    }
 
+   /*
    for (size_t c=0; c<send_cells.size(); ++c) {
          SpatialCell* spatial_cell = mpiGrid[send_cells[c]];
          spatial_cell->clear_compressed_data(popID);
    }
-   
-   std::cerr << "finished\n";
+   */
+
    // MPI_Barrier(MPI_COMM_WORLD);
    // cout << "end update_remote_mapping_contribution, dimension = " << dimension << ", direction = " << direction << endl;
    // MPI_Barrier(MPI_COMM_WORLD);
